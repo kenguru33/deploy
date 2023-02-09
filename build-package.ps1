97e081f8cba31a056a10170a801033923b364c62
Bernt <bernt.anker@rs.no>
2023-02-09 01:01:45 +0100

    build script all or single package

--- build-package.ps1
+++ build-package.ps1
@@ -2,11 +2,28 @@ $buildPath = Resolve-Path -Path "$PSScriptRoot/build"
 $packagePath = Resolve-Path -Path "$PSScriptRoot/packages"
 $package = $args[0]
 
-Write-Information "Creating intune package $package in $buildPath "
 # Check if command exist IntuneWinAppUtil.exe exist
 if (-not(Get-Command IntuneWinAppUtil.exe -errorAction SilentlyContinue)) {
     Write-Error "IntuneWinAppUtil.exe not found. "
     Write-Error "Please install the IntuneWinAppUtil.exe from https://docs.microsoft.com/en-us/intune/apps/apps-win32-app-management"
     exit 1
 }
-IntuneWinAppUtil.exe -c $packagePath\$package -s $package -o $buildPath
+
+Write-Host $package
+# If arg 1 is * loop thorugh all packages in packages folder
+if ($package -eq "*") {
+    $packages = Get-ChildItem -Path $packagePath -Directory -Name
+    Write-Host $packages  
+    foreach ($package in $packages) {
+        Write-Information "Creating intune package $package in $buildPath "
+        IntuneWinAppUtil.exe -c $packagePath/$package -s $package -o $buildPath -q
+    }
+}
+else {
+    Write-Information "Creating intune package $package in $buildPath "
+    IntuneWinAppUtil.exe -c $packagePath\$package -s $package -o $buildPath -q
+}
+
+
+
+
