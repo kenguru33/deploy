f509f85e82ee99473d79f68c5bac10a0205e514e
Bernt Anker <bernt.anker@rs.no>
2023-02-21 23:15:00 +0100

    powershell profile in core

--- chocolatey/packages/powershell-core/install.ps1
+++ chocolatey/packages/powershell-core/install.ps1
@@ -1,8 +1,14 @@
-    $packageName = "powershell-core"
-    $logPath = "c:\RS-MEM\$packageName-install.log"
-    Start-Transcript -Path $logPath -Force
-    # Set Chocolatey path
-    $env:Path = "$env:Path;C:\ProgramData\chocolatey\bin"
-    # Install Chocolatey package
-    choco install -y $packageName
-    Stop-Transcript
+$packageName = "powershell-core"
+$logPath = "c:\RS-MEM\$packageName-install.log"
+Start-Transcript -Path $logPath -Force
+# Set Chocolatey path
+$env:Path = "$env:Path;C:\ProgramData\chocolatey\bin"
+# Install Chocolatey package
+choco install -y $packageName
+
+# Set PowerShell 7 profile path
+$PS7_PROFILE_PATH = "C:\Program Files\powershell\7\profile.ps1"
+# Copy profile.ps1 to PowerShell 7 profile path
+Copy-Item -Path "$PSScriptRoot\profile.ps1" -Destination $PS7_PROFILE_PATH -Force
+
+Stop-Transcript
