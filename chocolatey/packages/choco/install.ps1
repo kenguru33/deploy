ee90d774b3113570dcf638daf71ffacebea8e510
Bernt <bernt.anker@rs.no>
2023-02-22 17:09:23 +0100

    Added update packages as scheduled task

--- chocolatey/packages/choco/install.ps1
+++ chocolatey/packages/choco/install.ps1
@@ -12,7 +12,23 @@ try {
         [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072 
         Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
     }
+   
+    # Create choco update scheduled task
+    $taskPath = "\Redningsselskapet\"
+    $taskName = "Choco Update"
+    $task = Get-ScheduledTask -TaskName $taskName -TaskPath $taskPath -ErrorAction SilentlyContinue -OutVariable task 
+    if (($task)) {
+        Write-Host "Scheduled task already exists"
+        Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
+        Write-Host "Deleted schedule task $taskName"
+    }
+
+    $action = New-ScheduledTaskAction -Execute "choco" -Argument "upgrade all -y"
+    $trigger = New-ScheduledTaskTrigger -Daily -At "11:30 AM" 
+    $settings = New-ScheduledTaskSettingsSet -RunOnlyIfNetworkAvailable
+    $principal = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Brukere" -RunLevel Highest
     
+    Register-ScheduledTask -Action $action -Trigger $trigger -TaskPath $taskPath -TaskName $taskName -Description "Updates all chocolatey packages" -Settings $settings -Principal $principal
     exit 0
 }
 catch { exit 1618 }
--- chocolatey/packages/choco/uninstall.ps1
+++ chocolatey/packages/choco/uninstall.ps1
@@ -5,3 +5,13 @@ $env:Path = "$env:Path;C:\ProgramData\chocolatey\bin"
 
 # Uninstall Chocolatey packages
 choco uninstall -y $packageName
+
+# Create choco update scheduled task
+$taskPath = "\Redningsselskapet\"
+$taskName = "Choco Update"
+$task = Get-ScheduledTask -TaskName $taskName -TaskPath $taskPath -ErrorAction SilentlyContinue -OutVariable task 
+if (($task)) {
+    Write-Host "Scheduled task already exists"
+    Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
+    Write-Host "Deleted schedule task $taskName"
+}
